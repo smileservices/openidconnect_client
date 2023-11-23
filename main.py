c3d39cae534d3310b0bbdac66523d9545d9aa9e9
vladimir.gorea <vladimir.gorea@lionstep.com>
2023-11-23 14:19:37 +0800

    get token handle error response

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from starlette.config import Config
 from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import HTTPException
 import requests
 import json
 from typing import Annotated
@@ -51,12 +52,26 @@ async def handle_code(
     }
     headers = {"Accept": "application/json"}
     provider_config = requests.get(config.get("provider")).json()
-    response = requests.post(provider_config["token_endpoint"], json=data, headers=headers).json()
-    auth_header = f'{response["token_type"]} {response["access_token"]}'
-    response_userdata = requests.get(provider_config["userinfo_endpoint"], headers={"authorization": auth_header}).json()
-    response_user_socials = requests.get(provider_config["user_socials_endpoint"], headers={"authorization": auth_header}).json()
-    response_user_reviews = requests.get(provider_config["user_reviews_endpoint"], headers={"authorization": auth_header}).json()
-    response_user_trust = requests.get(provider_config["user_trust_endpoint"], headers={"authorization": auth_header}).json()
+    response = requests.post(provider_config["token_endpoint"], json=data, headers=headers)
+    response_data = response.json()
+    if response.status_code != 200:
+        return templates.TemplateResponse(
+            "error.html",
+            {
+                **config,
+                "provider_config": provider_config,
+                "request": request, "error": response_data["detail"]
+            }
+        )
+    auth_header = f'{response_data["token_type"]} {response_data["access_token"]}'
+    response_userdata = requests.get(provider_config["userinfo_endpoint"],
+                                     headers={"authorization": auth_header}).json()
+    response_user_socials = requests.get(provider_config["user_socials_endpoint"],
+                                         headers={"authorization": auth_header}).json()
+    response_user_reviews = requests.get(provider_config["user_reviews_endpoint"],
+                                         headers={"authorization": auth_header}).json()
+    response_user_trust = requests.get(provider_config["user_trust_endpoint"],
+                                       headers={"authorization": auth_header}).json()
     return templates.TemplateResponse(
         "token_response.html",
         {
